1018f1b18b30a3bb247bd5459531cb22e83f0b53
Jessica Robinson <jessica.polansky@gmail.com>
2018-05-08 00:03:39 +0000

    adding jQuery exercises to github

---
+++ new.js
@@ -0,0 +1,29 @@
+$( document ).ready(function(e) {
+    // $('html').keydown(function(e) {
+    //     console.log('e', e);
+    //     console.log('e.keyCode', e.keyCode);
+    //     // console.log('e.key', e.key);
+    //     // console.log('e.originalEvent.code', e.originalEvent.code);
+    // });
+    link href="path/to/lightbox.css" rel="stylesheet">
+<script src="path/to/lightbox.js"></script>
+});
+//   var obj = { name: 'Jess' };
+//     $('li a').click(obj, function(e) {
+//         e.preventDefault();
+//         alert('Hi ' + obj.name);
+//     });
+// // $('#the-button').click(function() {
+// // $('li a').fadeOut(10000, function() {
+// // $('li').remove();
+// // });
+// // });
+// $('ul').on('click', 'li', function(e) {
+//         e.preventDefault();
+//         alert($(this).text());
+//     });
+//     $('#the-button').click(function() {
+//         $('.list').append('<li>Howdy!</li>');
+//     });
+
+    
